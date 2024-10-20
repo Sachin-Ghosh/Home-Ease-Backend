96d2c7d3f9babe6db8f052679b0f89a62ae6dba9
SACHIN <ghoshsachin2301@gmail.com>
2024-10-21 01:58:50 +0530

    made some changes

--- controllers/serviceController.js
+++ controllers/serviceController.js
@@ -9,7 +9,7 @@ const fs = require('fs').promises;
 // Set up multer for file uploads
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'uploads'); // Specify the upload directory
+        cb(null, 'uploads/'); // Specify the upload directory
     },
     filename: (req, file, cb) => {
         cb(null, `${Date.now()}-${file.originalname}`); // Create a unique filename
